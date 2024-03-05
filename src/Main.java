056d3a9056be5d0c0448b880c72330bdb03e815b
Ceiberon <ethem_641@hotmail.com>
2024-03-06 01:53:18 +0300

    MineSwep metodunun adını MineSweeper olarak değiştirildi

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        MineSwep mineSweeper = new MineSwep();
+        MineSweeper mineSweeper = new MineSweeper();
 
     }
 }
