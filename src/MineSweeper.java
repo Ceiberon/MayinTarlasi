ba31f06f2610832d25eccf0775fc8eb8b6746952
Ceiberon <ethem_641@hotmail.com>
2024-03-07 17:15:21 +0300

    MineSwep metodunun adını MineSweeper olarak değiştirildi

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class MineSweeper {
     Scanner scanner = new Scanner(System.in);
     int totalRows, totalColumns, userColumn, userRow;
